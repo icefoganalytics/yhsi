2eb8d5f1d2dfe20587d61135888d2037752907cf
Caleb Burke <calebburke91@gmail.com>
2026-03-24 14:38:40 -0700

    misc changes to support new db

--- api/config.ts
+++ api/config.ts
@@ -1,5 +1,8 @@
 import * as path from 'path';
 import * as dotenv from 'dotenv';
+import logger from '@/utils/logger';
+
+export const NODE_ENV = process.env.NODE_ENV || 'development';
 
 let dotEnvPath;
 switch (process.env.NODE_ENV) {
@@ -12,12 +15,16 @@ switch (process.env.NODE_ENV) {
 	default:
 		dotEnvPath = `.env.development`;
 }
+
 dotenv.config({ path: dotEnvPath });
+
+if (process.env.NODE_ENV !== 'test') {
+	logger.info('Loading env: ', dotEnvPath);
+}
 
 export const API_PORT = parseInt(process.env.API_PORT || '3000');
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'localhost:8080';
 export const AUTH_REDIRECT = process.env.AUTH_REDIRECT || process.env.FRONTEND_URL || '';
-export const NODE_ENV = process.env.NODE_ENV;
 
 export const DB_NAME = process.env.DB_NAME || '';
 export const DB_USER = process.env.DB_USER || '';
@@ -25,10 +32,13 @@ export const DB_PASS = process.env.DB_PASS || '';
 export const DB_HOST = process.env.DB_HOST || '';
 export const DB_PORT = process.env.DB_PORT || '1433';
 
+export const DEFAULT_LOG_LEVEL = process.env.DEFAULT_LOG_LEVEL || 'debug';
+
 export const CLIENT_ID = process.env.CLIENT_ID || '';
 export const ISSUER_BASE_URL = process.env.ISSUER_BASE_URL || '';
 export const AUTH_DB_CONNECTION = process.env.AUTH_DB_CONNECTION || '';
 
+// Keep in sync with api/src/db/db-client.ts
 export const DB_CONFIG = {
 	client: 'mssql',
 	connection: {
@@ -39,14 +49,14 @@ export const DB_CONFIG = {
 		port: parseInt(DB_PORT),
 	},
 	migrations: {
-		directory: path.resolve(__dirname, './data/migrations'),
+		directory: path.resolve(__dirname, './db/migrations'),
 		extension: 'ts',
-		stub: path.resolve(__dirname, './data/templates/sample-migration.ts'),
+		stub: path.resolve(__dirname, './db/templates/sample-migration.ts'),
 	},
 	seeds: {
-		directory: path.resolve(__dirname, `./data/seeds/${NODE_ENV}`),
+		directory: path.resolve(__dirname, `./db/seeds/${NODE_ENV}`),
 		extension: 'ts',
-		stub: path.resolve(__dirname, './data/templates/sample-seed.ts'),
+		stub: path.resolve(__dirname, './db/templates/sample-seed.ts'),
 	},
 };
 
